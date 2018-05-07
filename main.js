4d329b1e634622a8250ee68dbbeea62315a2271c
ned eisenberg <ned.eisenberg@gmail.com>
2018-05-06 22:47:30 -0400

    it should work now

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
 function pop_show(){
   document.getElementById('popup_div').style.display="block";
+}
+
+function cursor_show(){
+  document.getElementById('cursor_div').style.visibility="visible";
 }
